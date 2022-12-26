9a81ba4b90fc118bbe69675925de3dde53afeade
Obie <obie.tizon@gmail.com>
2022-12-26 17:54:42 +0800

    refactored dictionary to pipeline operator

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,8 +1,9 @@
 defmodule Dictionary do
 
   def word_list do 
-    words = File.read!("assets/words.txt")
-    String.split(words, ~r/\n/, trim: true)
+    "assets/words.txt" #created an assets directory in our project and download words.txt into it
+    |> File.read!()
+    |> String.split(~r/\n/, trim: true)
   end
 
   def random_word do 
