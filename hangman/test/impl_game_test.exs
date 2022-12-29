388cd2fcc612d7a9102702c249dc15764258b540
Obie <obie.tizon@gmail.com>
2022-12-30 00:44:09 +0800

    made test for IMPL Game module

---
+++ hangman/test/impl_game_test.exs
@@ -0,0 +1,25 @@
+defmodule HangmanImplGameTest do
+  use ExUnit.Case
+  doctest Hangman
+
+  alias Hangman.Impl.Game
+
+  test "new game returns structure" do
+    game = Game.new_game
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "new game returns correct word" do
+    game = Game.new_game("combat")
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert game.letters == ["c", "o", "m", "b", "a", "t"]
+  end
+
+
+
+end
