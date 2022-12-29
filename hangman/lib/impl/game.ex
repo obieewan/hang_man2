132ad164bc096f3a51369fc17916b26d4982cca3
Obie <obie.tizon@gmail.com>
2022-12-29 23:41:32 +0800

    change module name by using __MODULE__ for idiomatic elixir

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,7 +1,16 @@
-defmodule Hangman.Impl.Game do
+defmodule Hangman.Impl.Game do 
 
 
   #for defstruct: the name of the struct is the module name defstruct(
+  # logical structure of what we are returning
+  #we'll be able to access the type of this module as Hangman.Impl.Game.t
+  @type t :: %__MODULE__{
+    turns_left: integer, 
+    game_state: Hangman.state,
+    letters: list(String.t),
+    used: MapSet.t(String.t)
+  }
+
   defstruct(
     turns_left: 7, 
     game_state: :initializing,
@@ -9,7 +18,7 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )  
   def new_game do
-    %Hangman.Impl.Game{
+    %__MODULE__{
     letters: Dictionary.random_word |> String.codepoints 
     }
   end
