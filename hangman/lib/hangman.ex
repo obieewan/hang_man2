4a41c39368a8983d6d7bed9f44e0e5cf38228495
Obie <obie.tizon@gmail.com>
2022-12-30 12:31:22 +0800

    modified for clean code

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,21 +1,17 @@
 defmodule Hangman do #API module
 
   alias Hangman.Impl.Game #aliases the last word in the module chain
+  alias Hangman.Type
 
-  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  #opaque defines a type not usable outside module
   @opaque game :: Game.t #any is base of all types any single type compatible with any
-  @type tally :: %{
-    turns_left: integer,
-    game_state:  state,
-    letters: list(String.t),
-    used: list(String.t),
-  }
+  
 
   @spec new_game() :: game
   defdelegate new_game, to: Game #clear shows that the actual implementation of new game is in the Game module
   #makes it explicit that this is referrring that this is just an API
 
-  @spec make_move(game, String.t) :: {game, tally} #expected to receive guess as string
+  @spec make_move(game, String.t) :: {game, Type.tally} #expected to receive guess as string
   def make_move(game, guess) do
   end
 
--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,12 +1,14 @@
 defmodule Hangman.Impl.Game do 
 
+  alias Hangman.Type
+
 
   #for defstruct: the name of the struct is the module name defstruct(
   # logical structure of what we are returning
   #we'll be able to access the type of this module as Hangman.Impl.Game.t
   @type t :: %__MODULE__{
     turns_left: integer, 
-    game_state: Hangman.state,
+    game_state: Type.state,
     letters: list(String.t),
     used: MapSet.t(String.t)
   }
@@ -18,10 +20,14 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )  
 
+  #spec is type of the module t @type
+  @spec new_game() :: t
   def new_game do
     new_game(Dictionary.random_word)
   end
   
+  #this expects a word the from dictionary as string as type
+  @spec new_game(String.t) :: t
   def new_game(word) do
     %__MODULE__{
     letters: word |> String.codepoints 
