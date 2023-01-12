89313f1312bfcc3efbdf319dacec71decb431513
Obie <obie.tizon@gmail.com>
2023-01-13 05:18:25 +0800

    edited random function in the server and should be the same function name in the API

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,17 +1,21 @@
 defmodule Dictionary do
 
-  alias Dictionary.Impl.Wordlist
+  #not gonna delegate to the implementation anymore but to runtime
+  # 
+  alias Dictionary.Runtime.Server
 
   # opaque type to that the public will not know how dictionary
   # represented
-  @opaque t :: WordList
+  @opaque t :: Server.t
 
-  # start will start with the word_list function
-  @spec start() :: t
-  defdelegate start, to: Wordlist, as: :word_list
+  # start will start_link in the server and the return of the agent is
+  # specified @spec
+  @spec start_link() :: { :ok, t }
+  defdelegate start_link, to: Server # same function name in the server so alias is not needed
 
+  #function name  should be the same in the server to avoid alias
   @spec random_word(t) :: String.t
-  defdelegate random_word(word_list), to: Wordlist
+  defdelegate random_word(word_list), to: Server
   
 
 
--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,12 +1,14 @@
 defmodule Dictionary.Runtime.Server do
 
+  #this the type of the runtime server
+  @type t :: pid()
   alias Dictionary.Impl.Wordlist
 
   def start_link() do
     Agent.start_link(&Wordlist.word_list/0)
   end
 
-  def random(_word = pid) do
+  def random_word(_word = pid) do
     Agent.get(pid, &Wordlist.random_word/1)
   end
 end
